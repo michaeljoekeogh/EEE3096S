0920572cbdb52a187a76380a2a053f0452e4059e
michaeljoekeogh <141039758+michaeljoekeogh@users.noreply.github.com>
2023-10-16 16:47:31 +0200

    Add files via upload

--- main.c
+++ main.c
@@ -24,7 +24,6 @@
 #include <stdio.h>
 #include "stm32f0xx.h"
 #include <lcd_stm32f0.c>
-
 /* USER CODE END Includes */
 
 /* Private typedef -----------------------------------------------------------*/
@@ -34,7 +33,10 @@
 
 /* Private define ------------------------------------------------------------*/
 /* USER CODE BEGIN PD */
-
+// TODO: Add values for below variables
+#define NS 128        // Number of samples in LUT
+#define TIM2CLK 8000000  // STM Clock frequency
+#define F_SIGNAL 500 // Frequency of output analog signal
 /* USER CODE END PD */
 
 /* Private macro -------------------------------------------------------------*/
@@ -43,32 +45,39 @@
 /* USER CODE END PM */
 
 /* Private variables ---------------------------------------------------------*/
-ADC_HandleTypeDef hadc;
+TIM_HandleTypeDef htim2;
 TIM_HandleTypeDef htim3;
+DMA_HandleTypeDef hdma_tim2_ch1;
 
 /* USER CODE BEGIN PV */
-uint32_t prev_millis = 0;
-uint32_t curr_millis = 0;
-uint32_t delay_t = 500; // Initialise delay to 500ms
-uint32_t adc_val;
+// TODO: Add code for global variables, including LUTs
+
+uint32_t sin_LUT[NS] = {512,537,562,587,611,636,660,684,707,730,753,774,796,816,836,855,873,890,907,922,937,950,963,974,984,993,1001,1008,1013,1017,1021,1022,1023,1022,1021,1017,1013,1008,1001,993,984,974,963,950,937,922,907,890,873,855,836,816,796,774,753,730,707,684,660,636,611,587,562,537,512,486,461,436,412,387,363,339,316,293,270,249,227,207,187,168,150,133,116,101,86,73,60,49,39,30,22,15,10,6,2,1,0,1,2,6,10,15,22,30,39,49,60,73,86,101,116,133,150,168,187,207,227,249,270,293,316,339,363,387,412,436,461,486};
+
+uint32_t saw_LUT[NS] = {0,8,16,24,32,40,48,56,64,72,81,89,97,105,113,121,129,137,145,153,161,169,177,185,193,201,209,217,226,234,242,250,258,266,274,282,290,298,306,314,322,330,338,346,354,362,371,379,387,395,403,411,419,427,435,443,451,459,467,475,483,491,499,507,516,524,532,540,548,556,564,572,580,588,596,604,612,620,628,636,644,652,661,669,677,685,693,701,709,717,725,733,741,749,757,765,773,781,789,797,806,814,822,830,838,846,854,862,870,878,886,894,902,910,918,926,934,942,951,959,967,975,983,991,999,1007,1015,1023};
+
+uint32_t triangle_LUT[NS] = {0,8,16,24,32,41,49,57,65,73,81,89,97,106,114,122,130,138,146,154,162,171,179,187,195,203,211,219,227,235,244,252,260,268,276,284,292,300,309,317,325,333,341,349,357,365,373,382,390,398,406,414,422,430,438,447,455,463,471,479,487,495,503,512,512,503,495,487,479,471,463,455,447,438,430,422,414,406,398,390,382,373,365,357,349,341,333,325,317,309,300,292,284,276,268,260,252,244,235,227,219,211,203,195,187,179,171,162,154,146,138,130,122,114,106,97,89,81,73,65,57,49,41,32,24,16,8,0};
+
+// TODO: Equation to calculate TIM2_Ticks
+uint32_t TIM2_Ticks = TIM2CLK / (F_SIGNAL * NS); // How often to write new LUT value
+uint32_t DestAddress = (uint32_t) &(TIM3->CCR3); // Write LUT TO TIM3->CCR3 to modify PWM duty cycle
+
 /* USER CODE END PV */
 
 /* Private function prototypes -----------------------------------------------*/
 void SystemClock_Config(void);
 static void MX_GPIO_Init(void);
-static void MX_ADC_Init(void);
+static void MX_DMA_Init(void);
+static void MX_TIM2_Init(void);
 static void MX_TIM3_Init(void);
 
 /* USER CODE BEGIN PFP */
 void EXTI0_1_IRQHandler(void);
-void writeLCD(char *char_in);
-uint32_t pollADC(void);
-uint32_t ADCtoCCR(uint32_t adc_val);
 /* USER CODE END PFP */
 
 /* Private user code ---------------------------------------------------------*/
 /* USER CODE BEGIN 0 */
-char adc_buffer[16];
+
 /* USER CODE END 0 */
 
 /**
@@ -78,7 +87,6 @@ char adc_buffer[16];
 int main(void)
 {
   /* USER CODE BEGIN 1 */
-
   /* USER CODE END 1 */
 
   /* MCU Configuration--------------------------------------------------------*/
@@ -87,6 +95,7 @@ int main(void)
   HAL_Init();
 
   /* USER CODE BEGIN Init */
+  init_LCD();
   /* USER CODE END Init */
 
   /* Configure the system clock */
@@ -97,36 +106,38 @@ int main(void)
 
   /* Initialize all configured peripherals */
   MX_GPIO_Init();
-  MX_ADC_Init();
+  MX_DMA_Init();
+  MX_TIM2_Init();
   MX_TIM3_Init();
 
   /* USER CODE BEGIN 2 */
-  init_LCD();
+  // TODO: Start TIM3 in PWM mode on channel 3
+  HAL_TIM_PWM_Start(&htim3, TIM_CHANNEL_3);
 
-  // PWM setup
-  uint32_t CCR = 0;
-  HAL_TIM_PWM_Start(&htim3, TIM_CHANNEL_3); // Start PWM on TIM3 Channel 3
+
+  // TODO: Start TIM2 in Output Compare (OC) mode on channel 1.
+  HAL_TIM_OC_Start(&htim2, TIM_CHANNEL_1);
+
+
+  // TODO: Start DMA in IT mode on TIM2->CH1; Source is LUT and Dest is TIM3->CCR3; start with Sine LUT
+  HAL_DMA_Start_IT(&hdma_tim2_ch1, (uint32_t)sin_LUT, DestAddress, NS);
+
+
+  // TODO: Write current waveform to LCD ("Sine")
+
+
+  delay(3000);
+  lcd_command(CLEAR);
+  lcd_putstring("Sine");
+  // TODO: Enable DMA (start transfer from LUT to CCR)
+  __HAL_TIM_ENABLE_DMA(&htim2, TIM_DMA_CC1);
+
   /* USER CODE END 2 */
 
   /* Infinite loop */
   /* USER CODE BEGIN WHILE */
   while (1)
   {
-	// Toggle LED0
-	HAL_GPIO_TogglePin(GPIOB, LED7_Pin);
-
-	// ADC to LCD; TODO: Read POT1 value and write to LCD
-	uint32_t adc_value = pollADC();
-	char adc_string[32];
-	sprintf(adc_string, "ADC Value: %d", adc_value);
-	writeLCD(adc_string);
-	// Update PWM value; TODO: Get CRR
-	adc_val = pollADC();
-	CCR = ADCtoCCR(adc_val);
-	__HAL_TIM_SetCompare(&htim3, TIM_CHANNEL_3, CCR);
-
-	// Wait for delay ms
-	HAL_Delay (delay_t);
     /* USER CODE END WHILE */
 
     /* USER CODE BEGIN 3 */
@@ -152,14 +163,6 @@ void SystemClock_Config(void)
 
   }
   LL_RCC_HSI_SetCalibTrimming(16);
-  LL_RCC_HSI14_Enable();
-
-   /* Wait till HSI14 is ready */
-  while(LL_RCC_HSI14_IsReady() != 1)
-  {
-
-  }
-  LL_RCC_HSI14_SetCalibTrimming(16);
   LL_RCC_SetAHBPrescaler(LL_RCC_SYSCLK_DIV_1);
   LL_RCC_SetAPB1Prescaler(LL_RCC_APB1_DIV_1);
   LL_RCC_SetSysClkSource(LL_RCC_SYS_CLKSOURCE_HSI);
@@ -176,62 +179,63 @@ void SystemClock_Config(void)
   {
     Error_Handler();
   }
-  LL_RCC_HSI14_EnableADCControl();
 }
 
 /**
-  * @brief ADC Initialization Function
+  * @brief TIM2 Initialization Function
   * @param None
   * @retval None
   */
-static void MX_ADC_Init(void)
+static void MX_TIM2_Init(void)
 {
 
-  /* USER CODE BEGIN ADC_Init 0 */
-  /* USER CODE END ADC_Init 0 */
+  /* USER CODE BEGIN TIM2_Init 0 */
 
-  ADC_ChannelConfTypeDef sConfig = {0};
+  /* USER CODE END TIM2_Init 0 */
 
-  /* USER CODE BEGIN ADC_Init 1 */
+  TIM_ClockConfigTypeDef sClockSourceConfig = {0};
+  TIM_MasterConfigTypeDef sMasterConfig = {0};
+  TIM_OC_InitTypeDef sConfigOC = {0};
 
-  /* USER CODE END ADC_Init 1 */
+  /* USER CODE BEGIN TIM2_Init 1 */
 
-  /** Configure the global features of the ADC (Clock, Resolution, Data Alignment and number of conversion)
-  */
-  hadc.Instance = ADC1;
-  hadc.Init.ClockPrescaler = ADC_CLOCK_ASYNC_DIV1;
-  hadc.Init.Resolution = ADC_RESOLUTION_12B;
-  hadc.Init.DataAlign = ADC_DATAALIGN_RIGHT;
-  hadc.Init.ScanConvMode = ADC_SCAN_DIRECTION_FORWARD;
-  hadc.Init.EOCSelection = ADC_EOC_SINGLE_CONV;
-  hadc.Init.LowPowerAutoWait = DISABLE;
-  hadc.Init.LowPowerAutoPowerOff = DISABLE;
-  hadc.Init.ContinuousConvMode = DISABLE;
-  hadc.Init.DiscontinuousConvMode = DISABLE;
-  hadc.Init.ExternalTrigConv = ADC_SOFTWARE_START;
-  hadc.Init.ExternalTrigConvEdge = ADC_EXTERNALTRIGCONVEDGE_NONE;
-  hadc.Init.DMAContinuousRequests = DISABLE;
-  hadc.Init.Overrun = ADC_OVR_DATA_PRESERVED;
-  if (HAL_ADC_Init(&hadc) != HAL_OK)
+  /* USER CODE END TIM2_Init 1 */
+  htim2.Instance = TIM2;
+  htim2.Init.Prescaler = 0;
+  htim2.Init.CounterMode = TIM_COUNTERMODE_UP;
+  htim2.Init.Period = TIM2_Ticks - 1;
+  htim2.Init.ClockDivision = TIM_CLOCKDIVISION_DIV1;
+  htim2.Init.AutoReloadPreload = TIM_AUTORELOAD_PRELOAD_ENABLE;
+  if (HAL_TIM_Base_Init(&htim2) != HAL_OK)
   {
     Error_Handler();
   }
-
-  /** Configure for the selected ADC regular channel to be converted.
-  */
-  sConfig.Channel = ADC_CHANNEL_6;
-  sConfig.Rank = ADC_RANK_CHANNEL_NUMBER;
-  sConfig.SamplingTime = ADC_SAMPLETIME_1CYCLE_5;
-  if (HAL_ADC_ConfigChannel(&hadc, &sConfig) != HAL_OK)
+  sClockSourceConfig.ClockSource = TIM_CLOCKSOURCE_INTERNAL;
+  if (HAL_TIM_ConfigClockSource(&htim2, &sClockSourceConfig) != HAL_OK)
   {
     Error_Handler();
   }
-  /* USER CODE BEGIN ADC_Init 2 */
-  ADC1->CR |= ADC_CR_ADCAL;
-  while(ADC1->CR & ADC_CR_ADCAL);			// Calibrate the ADC
-  ADC1->CR |= (1 << 0);						// Enable ADC
-  while((ADC1->ISR & (1 << 0)) == 0);		// Wait for ADC ready
-  /* USER CODE END ADC_Init 2 */
+  if (HAL_TIM_OC_Init(&htim2) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  sMasterConfig.MasterOutputTrigger = TIM_TRGO_RESET;
+  sMasterConfig.MasterSlaveMode = TIM_MASTERSLAVEMODE_DISABLE;
+  if (HAL_TIMEx_MasterConfigSynchronization(&htim2, &sMasterConfig) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  sConfigOC.OCMode = TIM_OCMODE_TIMING;
+  sConfigOC.Pulse = 0;
+  sConfigOC.OCPolarity = TIM_OCPOLARITY_HIGH;
+  sConfigOC.OCFastMode = TIM_OCFAST_DISABLE;
+  if (HAL_TIM_OC_ConfigChannel(&htim2, &sConfigOC, TIM_CHANNEL_1) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  /* USER CODE BEGIN TIM2_Init 2 */
+
+  /* USER CODE END TIM2_Init 2 */
 
 }
 
@@ -257,9 +261,9 @@ static void MX_TIM3_Init(void)
   htim3.Instance = TIM3;
   htim3.Init.Prescaler = 0;
   htim3.Init.CounterMode = TIM_COUNTERMODE_UP;
-  htim3.Init.Period = 47999;
+  htim3.Init.Period = 1023;
   htim3.Init.ClockDivision = TIM_CLOCKDIVISION_DIV1;
-  htim3.Init.AutoReloadPreload = TIM_AUTORELOAD_PRELOAD_DISABLE;
+  htim3.Init.AutoReloadPreload = TIM_AUTORELOAD_PRELOAD_ENABLE;
   if (HAL_TIM_Base_Init(&htim3) != HAL_OK)
   {
     Error_Handler();
@@ -295,6 +299,22 @@ static void MX_TIM3_Init(void)
 }
 
 /**
+  * Enable DMA controller clock
+  */
+static void MX_DMA_Init(void)
+{
+
+  /* DMA controller clock enable */
+  __HAL_RCC_DMA1_CLK_ENABLE();
+
+  /* DMA interrupt init */
+  /* DMA1_Channel4_5_IRQn interrupt configuration */
+  HAL_NVIC_SetPriority(DMA1_Channel4_5_IRQn, 0, 0);
+  HAL_NVIC_EnableIRQ(DMA1_Channel4_5_IRQn);
+
+}
+
+/**
   * @brief GPIO Initialization Function
   * @param None
   * @retval None
@@ -302,7 +322,6 @@ static void MX_TIM3_Init(void)
 static void MX_GPIO_Init(void)
 {
   LL_EXTI_InitTypeDef EXTI_InitStruct = {0};
-  LL_GPIO_InitTypeDef GPIO_InitStruct = {0};
 /* USER CODE BEGIN MX_GPIO_Init_1 */
 /* USER CODE END MX_GPIO_Init_1 */
 
@@ -310,9 +329,6 @@ static void MX_GPIO_Init(void)
   LL_AHB1_GRP1_EnableClock(LL_AHB1_GRP1_PERIPH_GPIOF);
   LL_AHB1_GRP1_EnableClock(LL_AHB1_GRP1_PERIPH_GPIOA);
   LL_AHB1_GRP1_EnableClock(LL_AHB1_GRP1_PERIPH_GPIOB);
-
-  /**/
-  LL_GPIO_ResetOutputPin(LED7_GPIO_Port, LED7_Pin);
 
   /**/
   LL_SYSCFG_SetEXTISource(LL_SYSCFG_EXTI_PORTA, LL_SYSCFG_EXTI_LINE0);
@@ -330,14 +346,6 @@ static void MX_GPIO_Init(void)
   EXTI_InitStruct.Trigger = LL_EXTI_TRIGGER_RISING;
   LL_EXTI_Init(&EXTI_InitStruct);
 
-  /**/
-  GPIO_InitStruct.Pin = LED7_Pin;
-  GPIO_InitStruct.Mode = LL_GPIO_MODE_OUTPUT;
-  GPIO_InitStruct.Speed = LL_GPIO_SPEED_FREQ_LOW;
-  GPIO_InitStruct.OutputType = LL_GPIO_OUTPUT_PUSHPULL;
-  GPIO_InitStruct.Pull = LL_GPIO_PULL_NO;
-  LL_GPIO_Init(LED7_GPIO_Port, &GPIO_InitStruct);
-
 /* USER CODE BEGIN MX_GPIO_Init_2 */
   HAL_NVIC_SetPriority(EXTI0_1_IRQn, 0, 0);
   HAL_NVIC_EnableIRQ(EXTI0_1_IRQn);
@@ -345,73 +353,76 @@ static void MX_GPIO_Init(void)
 }
 
 /* USER CODE BEGIN 4 */
+int i;
 void EXTI0_1_IRQHandler(void)
 {
-	// TODO: Add code to switch LED7 delay frequency
-	static uint32_t last_button_press = 0;
-	    uint32_t current_time = HAL_GetTick();
+	// TODO: Debounce using HAL_GetTick()
+	static uint32_t lastDebounceTime = 0;
+	    static uint32_t debounceDelay = 50;  // Adjust this value for debouncing
 
-	    // Check if the interrupt was triggered by Button 0 
-	    if (__HAL_GPIO_EXTI_GET_IT(Button0_Pin) != RESET)
+	    uint32_t currentMillis = HAL_GetTick();
+
+	    // Check if the button press is debounced
+	    if (currentMillis - lastDebounceTime > debounceDelay)
 	    {
-	        // Check if it's been long enough since the last button press
-	        if (current_time - last_button_press >= 100) 
-	        {
-	            // Toggle the LED delay frequency between 1 Hz and 2 Hz
-	            if (delay_t == 1000) // Current delay is 1 Hz
-	            {
-	                delay_t = 500; // Set delay to 2 Hz (500 ms)
-	            }
-	            else // Current delay is 2 Hz
-	            {
-	                delay_t = 1000; // Set delay to 1 Hz (1000 ms)
-	            }
+	        // Debounce time has passed, process the button press
 
-	            // Update the last button press time
-	            last_button_press = current_time;
-	        }
+	        // Disable DMA transfer and abort it
+	        __HAL_TIM_DISABLE_DMA(&htim2, TIM_DMA_CC1);
+	        HAL_DMA_Abort_IT(&hdma_tim2_ch1);
 
-	        // Clear the interrupt flags for Button 0
-	        __HAL_GPIO_EXTI_CLEAR_IT(Button0_Pin);
+
+
+	        // Re-enable DMA with the new source address
+	        HAL_DMA_Start_IT(&hdma_tim2_ch1, DestAddress, (uint32_t)&(TIM3->CCR3), NS);
+
+	        // Update the debounce time
+	        lastDebounceTime = currentMillis;
 	    }
+
+	// TODO: Disable DMA transfer and abort IT, then start DMA in IT mode with new LUT and re-enable transfer
+	    //HAL_DMA_DISABLE(&hdma_tim2_ch1, (uint32_t)sin_LUT, DestAddress, NS);
+    	__HAL_TIM_DISABLE_DMA(&htim2, TIM_DMA_CC1);
+    	HAL_DMA_Abort_IT(&hdma_tim2_ch1);
+	    switch (i){
+	    	            case 0:
+	    	            	lcd_command(CLEAR);
+	    	                lcd_putstring("Saw");
+	    	                // Change to the next waveform (e.g., Sawtooth)
+	    	                //DestAddress = (uint32_t)saw_LUT;
+	    	                // TODO: Start DMA in& IT mode with the new LUT
+	    	                __HAL_TIM_ENABLE_DMA(&htim2, TIM_DMA_CC1);
+	    	                HAL_DMA_Start_IT(&hdma_tim2_ch1, saw_LUT, DestAddress, NS);
+	    	                i=1;
+	    	                break;
+	    	            case 1:
+							lcd_command(CLEAR);
+							lcd_putstring("Triangle");
+							// Change to the next waveform (e.g., Sawtooth)
+							//DestAddress = (uint32_t)triangle_LUT;
+							// TODO: Start DMA in IT mode with the new LUT
+							__HAL_TIM_ENABLE_DMA(&htim2, TIM_DMA_CC1);
+							HAL_DMA_Start_IT(&hdma_tim2_ch1, triangle_LUT, DestAddress, NS);
+							i=2;
+							break;
+
+	    	            case 2:
+							lcd_command(CLEAR);
+							lcd_putstring("Sine");
+							// Change to the next waveform (e.g., Sawtooth)
+							//DestAddress = (uint32_t)sin_LUT;
+							// TODO: Start DMA in IT mode with the new LUT
+						    __HAL_TIM_ENABLE_DMA(&htim2, TIM_DMA_CC1);
+						    HAL_DMA_Start_IT(&hdma_tim2_ch1, sin_LUT, DestAddress, NS);
+						    i=0;
+						    break;
+	    	            // Add more cases if needed
+	    	        }
+	// HINT: Consider using C's "switch" function to handle LUT changes
+
+
 
 	HAL_GPIO_EXTI_IRQHandler(Button0_Pin); // Clear interrupt flags
-}
-
-// TODO: Complete the writeLCD function
-void writeLCD(char *char_in){
-	delay(3000);
-    lcd_command(CLEAR);
-    lcd_putstring(char_in);
-}
-
-// Get ADC value
-uint32_t pollADC(void){
-  // TODO: Complete function body to get ADC value
-	uint32_t adc_value = 0;
-	    // Start ADC 
-	    HAL_ADC_Start(&hadc);
-	    // Wait conversion to complete
-	    if (HAL_ADC_PollForConversion(&hadc, HAL_MAX_DELAY) == HAL_OK) {
-	        // Read ADC value
-	        adc_value = HAL_ADC_GetValue(&hadc);
-	    }
-	    // Stop 
-	    HAL_ADC_Stop(&hadc);
-	    return adc_value;
-}
-
-// Calculate PWM CCR value
-uint32_t ADCtoCCR(uint32_t adc_val){
-  // TODO: Calculate CCR val using an appropriate equation
-	return (adc_val * (htim3.Init.Period + 1))/ 4096;
-
-}
-
-void ADC1_COMP_IRQHandler(void)
-{
-	adc_val = HAL_ADC_GetValue(&hadc); // read adc value
-	HAL_ADC_IRQHandler(&hadc); //Clear flags
 }
 /* USER CODE END 4 */
 
